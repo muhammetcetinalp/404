67d69d576982f5a5e6d12ffe22cdea17e53e15ea
Yusuf Demir <yusufdemrr@hotmail.com>
2025-03-15 17:36:10 +0300

    ilk commit

---
+++ backend/delivery_backend/src/test/java/com/backend/delivery_backend/DeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.delivery_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

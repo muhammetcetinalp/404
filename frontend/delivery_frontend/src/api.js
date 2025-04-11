5c9c5c02c083f460be8cdfec51247a357bf70d33
muhammetcetinalp <muhammedcetinalp35@gmail.com>
2025-04-11 03:55:51 +0300

    bu fixed by berkay

--- frontend/delivery_frontend/src/api.js
+++ frontend/delivery_frontend/src/api.js
@@ -23,7 +23,6 @@ api.interceptors.response.use(
     (error) => {
         if (error.response?.status === 401) {
             localStorage.clear();
-            window.location.href = '/login';
         }
         return Promise.reject(error);
     }
